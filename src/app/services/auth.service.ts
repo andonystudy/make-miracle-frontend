5d4a34fce979a98a856884cefcee65567f111979
jasson <jassoncaceres98@gmail.com>
2020-05-13 22:50:45 -0500

    agregando servicio auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,143 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Usuario } from '../models/usuario';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private _usuario: Usuario;
+  private _token: string;
+
+  constructor(private http: HttpClient) { }
+
+  public get usuario(): Usuario {
+
+    if (this._usuario != null) {
+      return this._usuario;
+    } else if (this._usuario == null && sessionStorage.getItem('usuario') != null) {
+      this._usuario = JSON.parse(sessionStorage.getItem('usuario')) as Usuario;
+      return this._usuario;
+    }
+
+
+    return new Usuario();
+  }
+
+
+  public get token(): string {
+
+    if (this._token != null) {
+      return this._token;
+
+    } else if (this._token === null && sessionStorage.getItem('token') != null) {
+      this._token = sessionStorage.getItem('token');
+      return this._token;
+    }
+
+    return null;
+
+  }
+
+  // login
+
+  login(usuario: Usuario): Observable<any> {
+
+    const urlEndPoint = 'https://localhost:8080/oauth/token';
+
+    // credenciales
+    const credenciales = btoa('makeamiracle' + ':' + '12345');
+
+
+    // configurando la cabecera donde pasaremos nuestras credenciales
+    const httpheaders = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + credenciales
+    });
+
+    let params = new URLSearchParams();
+
+    params.set('grant_type', 'password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+
+
+    console.log(params.toString());
+
+
+
+    return this.http.post<any>(urlEndPoint, params.toString(), { headers: httpheaders });
+  }
+
+
+
+
+  obtenerDatosdelToken(accessToken: string): any {
+    if (accessToken != null) {
+
+      return (JSON.parse(atob(accessToken.split(".")[1])));
+    }
+
+    return null;
+  }
+
+
+  guardarUsuario(accessToken: string): void {
+
+    let payload = this.obtenerDatosdelToken(accessToken);
+
+    this._usuario = new Usuario();
+    this._usuario.nombre = payload.Nombre;
+    this._usuario.apellido = payload.Apellido;
+    this._usuario.roles = payload.authorities;
+
+
+    sessionStorage.setItem('usuario', JSON.stringify(this._usuario));
+
+  }
+
+  guardarToken(accessToken: string): void {
+    // no necesitamos parsearlo a string ya que este metodo es un string
+    this._token = accessToken;
+    sessionStorage.setItem('token', accessToken);
+  }
+
+
+
+  // si esta authenticado el usuario con su respectivas credenciales
+
+  isAuthenticated(): boolean {
+    let payload = this.obtenerDatosdelToken(this._token);
+
+    if (payload != null && payload.user_name != null && payload.user_name.length > 0) {
+      return true;
+    }
+
+    return false;
+
+  }
+
+
+
+  hasRole(role: string): boolean {
+
+    if (this.usuario.roles.includes(role)) {
+      return true;
+    }
+
+    return false;
+  }
+
+
+  logout(): void {
+    this._token = null;
+    this._usuario = null;
+
+    // limpiamos el sessionStorage
+    sessionStorage.clear();
+  }
+
+
 }
